9e19646fa087f3e06ce74a9c1400229d01b766e4
Chad Hinds <chadrickj8@gmail.com>
2020-04-08 17:29:15 -0400

    milliseconds iteration

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,46 +1,68 @@
 class Chronometer {
   constructor() {
-    this.currentTime = 0;
-    this.intervalId = 0;
+    this.seconds = 0; //centiseconds ? 
+    this.milliSeconds = 0; // BONUS
+    this.intervalId = 0; //11  
   }
 
-  startClick(callback) {
-   this.intervalId = setInterval(() => {
-    //  stuff
-    this.currentTime += 1
-   }, 1000);
-   this.intervalId = this.intervalId
+  // starts the setInterval
+  startClick(printTimeFunc) {
+    this.intervalId = setInterval(() => {
+      console.log(this.seconds)
+      this.milliSeconds++
+      if (this.milliSeconds % 100 === 0) {
+        this.seconds += 1;
+      }
+      // if(printTimeFunc)
+      printTimeFunc()
+    }, 10)
   }
-  
+
+
+
   getMinutes() {
     // ... your code goes here
-    return (Math.floor(this.currentTime / 60));
+    return Math.floor(this.seconds / 60);
   }
 
   getSeconds() {
     // ... your code goes here
-    return (Math.floor(this.currentTime % 60))
+    return this.seconds % 60;
   }
 
-  twoDigitsNumber() {
+  getMilliSeconds() {
+    return this.milliSeconds % 100;
+  }
+
+  // this will return a string with two number characters (e.g. 01, 10, 35)
+  twoDigitsNumber(value) {
     // ... your code goes here
-   let num = this.currentTime.toFixed(0)
-   let newNum = "0"
-    if (num.length < 2) {
-      newNum += num
+    // if(this.seconds < 10){
+    //   return '0'+this.seconds.toString()
+    // } else if(this.seconds >= 10 && this.seconds < 100){
+    //   return this.seconds.toString()
+    // }
+
+    if (value < 10) {
+      return `0${value}`
+    } else {
+      return `${value}`
     }
-    return num
   }
 
   stopClick() {
     clearInterval(this.intervalId)
   }
+
   resetClick() {
-    // ... your code goes here
-    this.currentTime = 0
+    this.seconds = 0;
+    this.milliSeconds = 0;
   }
+
+  // this will return what is going to be showed under the splits (e.g. 01:12 )
   splitClick() {
     // ... your code goes here
-    
+    return `${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(this.getSeconds())}:${this.twoDigitsNumber(this.getMilliSeconds())}`
+    // return `${this.getMinutes()}:${this.getSeconds()}:${this.getMilliSeconds()}`
   }
 }
