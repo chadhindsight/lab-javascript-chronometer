478aca59ebb0d482d8128d9382dce84ccbdfbcc4
Chad Hinds <chadrickj8@gmail.com>
2020-04-08 12:14:53 -0400

    get two digits

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -5,26 +5,42 @@ class Chronometer {
   }
 
   startClick(callback) {
-    setInterval(() => {
-      
-    }, 1000);
+   this.intervalId = setInterval(() => {
+    //  stuff
+    this.currentTime += 1
+   }, 1000);
+   this.intervalId = this.intervalId
   }
+  
   getMinutes() {
     // ... your code goes here
+    return (Math.floor(this.currentTime / 60));
   }
+
   getSeconds() {
     // ... your code goes here
+    return (Math.floor(this.currentTime % 60))
   }
+
   twoDigitsNumber() {
     // ... your code goes here
+   let num = this.currentTime.toFixed(0)
+   let newNum = "0"
+    if (num.length < 2) {
+      newNum += num
+    }
+    return num
   }
+
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
   resetClick() {
     // ... your code goes here
+    this.currentTime = 0
   }
   splitClick() {
     // ... your code goes here
+    
   }
 }
